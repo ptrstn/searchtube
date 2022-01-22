2bd4585052dea6df37880be541fa6a5c17f8b5ef
Peter Stein <peterstein@protonmail.com>
2022-01-22 19:29:02 +0100

    Test extract_video

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -1,4 +1,4 @@
-from searchtube.extractors import extract_search_suggestions
+from searchtube.extractors import extract_search_suggestions, extract_video
 
 
 def test_extract_search_suggestions():
@@ -29,3 +29,78 @@ def test_extract_search_suggestions():
     assert suggestions[0] == "peter althof"
     assert suggestions[1] == "peter maffay"
     assert suggestions[13] == "peter rosegger"
+
+
+def test_extract_video():
+    item = {
+        "videoRenderer": {
+            "videoId": "6abc2FEa2P0",  # video_id
+            "title": {
+                "runs": [{"text": "A Video title"}],  # title
+            },
+            "publishedTimeText": {"simpleText": "vor 1 Tag"},  # published_time
+            "lengthText": {
+                "simpleText": "22:00",  # duration
+            },
+            "viewCountText": {"simpleText": "7.407 Aufrufe"},  # view_count
+            "ownerText": {
+                "runs": [
+                    {
+                        "text": "An author name",  # author
+                        "navigationEndpoint": {
+                            "commandMetadata": {
+                                "webCommandMetadata": {
+                                    "url": "/c/TheChannel",  # channel_url
+                                }
+                            },
+                        },
+                    }
+                ]
+            },
+        },
+    }
+
+    video = extract_video(item)
+
+    assert video["video_id"] == "6abc2FEa2P0"
+    assert video["title"] == "A Video title"
+    assert video["published_time"] == "vor 1 Tag"
+    assert video["duration"] == "22:00"
+    assert video["view_count"] == "7.407 Aufrufe"
+    assert video["author"] == "An author name"
+    assert video["channel_url"] == "/c/TheChannel"
+
+
+def test_incomplete_extract_video():
+    item = {
+        "videoRenderer": {
+            "videoId": "6abc2FEa2P0",  # video_id
+            "title": {
+                "runs": [{"text": "A Video title"}],  # title
+            },
+            "ownerText": {
+                "runs": [
+                    {
+                        "text": "An author name",  # author
+                        "navigationEndpoint": {
+                            "commandMetadata": {
+                                "webCommandMetadata": {
+                                    "url": "/c/TheChannel",  # channel_url
+                                }
+                            },
+                        },
+                    }
+                ]
+            },
+        },
+    }
+
+    video = extract_video(item)
+
+    assert video["video_id"] == "6abc2FEa2P0"
+    assert video["title"] == "A Video title"
+    assert video["published_time"] is None
+    assert video["duration"] is None
+    assert video["view_count"] is None
+    assert video["author"] == "An author name"
+    assert video["channel_url"] == "/c/TheChannel"
